32d6af8563c8add077b4d48f1f90a0b04f292f20
Завидей Дмитрий <144597993+Zavidei@users.noreply.github.com>
2025-04-14 14:27:54 +0300

    change task 8

--- task_8/main.js
+++ task_8/main.js
@@ -1,10 +1,30 @@
-const startButton = document.getElementById('startButton');
-const progressBar = document.getElementById('progressBar');
-const statusText = document.getElementById('statusText');
+let progressBar;
+let statusText;
+let startButton;
+let progressWrapper;
 
-let progress = 0;
+const createElements = () => {
+    progressWrapper = document.createElement('div');
+    startButton = document.createElement('button');
+    progressBar = document.createElement('div');
+    statusText = document.createElement('p');
+
+    progressBar.className = 'progress-bar';
+    progressWrapper.className = 'progress-wrapper';
+
+    startButton.textContent = 'Start Loading';
+    statusText.textContent = 'Progress: 0%';
+}
+
+const buildLoadingComponent = () => {
+    document.body.appendChild(startButton);
+    document.body.appendChild(progressWrapper);
+    progressWrapper.appendChild(progressBar);
+    document.body.appendChild(statusText);
+}
 
 function startLoading() {
+    let progress = 0;
     const loading = setInterval(() => {
         progress++;
         progressBar.style.width = `${progress}%`;
@@ -16,6 +36,9 @@ function startLoading() {
 
     }, 100);
 }
+
+createElements();
+buildLoadingComponent();
 
 startButton.addEventListener('click', startLoading);
 
